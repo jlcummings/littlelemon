75e10c8a63ee04245899bd78e66f43c4aa745dbf
Justin L. Cummings <Justin L. Cummings 666404+jlcummings@users.noreply.github.com>
2023-11-07 14:42:56 -0600

    Scaffold an ios app for mobile users

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Justin Cummings on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
